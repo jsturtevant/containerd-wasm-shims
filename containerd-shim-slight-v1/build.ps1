9f73aa8eda57ee66c395ae1bcc2bf9e2c80f16d2
James Sturtevant <jstur@microsoft.com>
2023-04-13 08:18:15 -0700

    working for demo
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- containerd-shim-slight-v1/build.ps1
+++ containerd-shim-slight-v1/build.ps1
@@ -1,3 +1,5 @@
+$env:OPENSSL_NO_VENDOR="true"
+$env:OPENSSL_DIR = "C:\Program Files\OpenSSL-Win64\"
 cargo build
 cp -force .\target\debug\containerd-shim-slight-v1.exe ../../containerd/bin/
 ls ../../containerd/bin/containerd-shim-slight-v1.exe
